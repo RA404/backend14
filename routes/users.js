1e5256334fb31dd3c4279a1d4c0548c9f19502ed
RA404 <Repin404@gmail.com>
2020-05-18 20:20:26 +0500

    added joi validation for password

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,22 @@
 const userRouter = require('express').Router();
 const { findAll, findUser, createUser, login } = require('../controllers/users');
 const auth = require('../middlewares/auth');
+const schemaSignUp = require('../schemas/signUp');
+const joi = require('joi');
+
+const validate = (schemaSignUp) => {
+  return (req, res, next) => {
+    joi.validate(req.body, schemaSignUp)
+      .then(() => next())
+      .catch((err) => {
+        return res.status(400).send({ message: err.message })
+      })
+  }
+}
 
 userRouter.get('/', auth.auth, findAll);
 userRouter.get('/:id', auth.auth, findUser);
-userRouter.post('/signup', createUser);
+userRouter.post('/signup', validate(schemaSignUp), createUser);
 userRouter.post('/signin', login);
 
 module.exports = userRouter;
