ab0b763ce302cd374cac2fb35d4300c8e1409bbb
RA404 <Repin404@gmail.com>
2020-05-26 17:24:45 +0500

    dotenv installed

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports.PORT = process.env.PORT || 3000;
 module.exports.DATABASE_URL = 'mongodb://localhost:27017/mestodb';
 module.exports.JWT_SECRET = process.env.JWT_SECRET || 'some-secret-key';
+module.exports.NODE_ENV = process.env.NODE_ENV || 'develop';
