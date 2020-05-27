3c22f4eb7d5c987784f3f67d9852145c2687f613
RA404 <Repin404@gmail.com>
2020-05-27 16:21:00 +0500

    fixed joi validation rules

--- schemas/cards.js
+++ schemas/cards.js
@@ -2,8 +2,9 @@ const joi = require('joi');
 const ErrorBadRequest = require('../errors/ErrorBadRequest');
 
 module.exports = joi.object().keys({
-  name: joi.string(),
-  link: joi.string().uri().error(() => new ErrorBadRequest({ message: 'Invalid url' })),
+  name: joi.string().required().min(2).max(30)
+    .error(() => new ErrorBadRequest({ message: 'The name must be longer than 2 and less than 30 characters' })),
+  link: joi.string().required().uri().error(() => new ErrorBadRequest({ message: 'Invalid url' })),
   owner: joi.string(),
   likes: joi.array(),
   createAt: joi.date(),
--- schemas/signUp.js
+++ schemas/signUp.js
@@ -2,9 +2,10 @@ const joi = require('joi');
 const ErrorBadRequest = require('../errors/ErrorBadRequest');
 
 module.exports = joi.object().keys({
-  name: joi.string(),
   about: joi.string(),
-  avatar: joi.string(),
-  email: joi.string().email().error(() => new ErrorBadRequest({ message: 'Invalid email' })),
+  name: joi.string().min(2).max(30).required()
+    .error(() => new ErrorBadRequest({ message: 'The name must be longer than 2 and less than 30 characters' })),
+  avatar: joi.string().required().uri().error(() => new ErrorBadRequest({ message: 'Invalid avatar link' })),
+  email: joi.string().required().email().error(() => new ErrorBadRequest({ message: 'Invalid email' })),
   password: joi.string().min(8).required().error(() => new ErrorBadRequest({ message: 'Password must be longer than 8 character' })),
 });
